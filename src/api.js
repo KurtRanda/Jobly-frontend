491ae99fcea5c933d1e18ee39f417331129bfae9
Kurt Randa <kurt.randa7@gmail.com>
2024-12-10 14:19:40 -0600

    api.js update

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "https://jobly-backend-ds7w.onrender.com";
+const BASE_URL = "https://jobly-backend-ds7w.onrender.com";
 
 /** API Class.
  *
